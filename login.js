6722b00e410eede1ae7cf237cc212fb88a0d418a
league_studios <victormitev70@gmail.com>
2022-05-30 13:19:36 -0400

    Update

--- login.js
+++
@@ -1,3 +0,0 @@
-const { redirect } = require("statuses");
-
-const url = "https://discord.com/api/oauth2/authorize?client_id=960192724949172234&redirect_uri=https%3A%2F%2Frobloxstudiocentral.xyz%2F&response_type=code&scope=identify%20email";
