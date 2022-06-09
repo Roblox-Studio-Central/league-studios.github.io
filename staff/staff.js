cb706e05f2028b9bd565602a769252bdca7ac7ff
league_studios <victormitev70@gmail.com>
2022-06-09 19:31:24 -0400

    New form

--- staff/staff.js
+++
@@ -1,3 +0,0 @@
-window.onload(
-    "https://discord.com/api/oauth2/authorize?client_id=971079334653755503&redirect_uri=http%3A%2F%2F127.0.0.1%3A5500%2Fstaff%2Fmain.html&response_type=code&scope=identify"
-)
